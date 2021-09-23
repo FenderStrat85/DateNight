a76b065bd52af9e2571fc31387e980b1738df392
Richard Barnes <rbarnes2@hotmail.co.uk>
2021-09-23 08:27:15 +0100

    added client folder with expo project. Added console logs to controller to confirm communication between front and back end

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,7 +3,7 @@ const User = require('../models/models');
 
 const create = async (req, res) => {
   const { email, password } = req.body;
-
+  console.log('inside create function');
   let salt = bcrypt.genSaltSync();
   let passwordHash = bcrypt.hashSync(password, salt);
 
@@ -16,21 +16,32 @@ const create = async (req, res) => {
       password: passwordHash,
     });
     req.session.uid = createUser._id;
+    console.log('user created big success', createUser);
     //need to change res.send to createUser.email
     res.status(200).send(createUser);
   }
 };
 
 const login = async (req, res) => {
+  console.log('inside login function');
+  console.log(req.body);
   try {
     const { email, password } = req.body;
+    console.log(email);
+    console.log(password);
     const user = await User.findOne({ email });
+    if (!user) {
+      console.log('user does not exist');
+      throw new Error();
+    }
     const validatedPassword = await bcrypt.compare(password, user.password);
 
     if (!validatedPassword) {
+      console.log('I do not exist');
       throw new Error();
     } else {
       req.session.uid = user._id;
+      console.log(user);
       res.status(200).send(user.restaurants);
     }
   } catch (error) {
