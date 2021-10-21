7f7f65869e26131f1af06110c4238fefc1bc0e8b
Richard Barnes <rbarnes2@hotmail.co.uk>
2021-10-21 15:44:37 +0100

    console.logs from server removed

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,12 +1,9 @@
 const { User } = require('../models/models');
 
 const authMiddleware = async (req, res, next) => {
-  console.log('inside auth');
-  console.log('req.body in auth', req.body);
   try {
     const { uid } = req.session;
     const user = await User.findOne({ _id: uid });
-    console.log(user);
     if (!user) throw new Error();
     req.user = user;
     next();
